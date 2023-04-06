75eb9190ca91a7dbe26382cb9f797d2758c03054
Tara Pogančev <tarapogancev@gmail.com>
2023-04-06 21:53:59 +0200

    fix: hide schedule

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   showCompanies: true,
   showSponsors: true,
-  showSchedule: true,
+  showSchedule: false,
   showOffers: true,
   spaceId: '3yzxhdb0m5bd',
   accessToken: 'zXfPWcOXtYWZtD-z4FnVUJ2eCF7Jv92EANW0l9UduDA',
