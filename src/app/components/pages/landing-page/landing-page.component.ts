0229baddfa45401d79127d98cbe703b41e201c84
marijaparezanin <marija.parezanin@mensa.ba>
2025-02-21 02:31:52 +0100

    [ADD] - Add new main page, add video background to activities and companies

--- src/app/components/pages/landing-page/landing-page.component.ts
+++ src/app/components/pages/landing-page/landing-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import {MatDialog} from "@angular/material/dialog";
+import lgZoom from "lightgallery/plugins/zoom";
+import {PolicyComponent} from "../about-konteh/policy/policy.component";
 
 @Component({
   selector: 'landing-page',
@@ -8,9 +11,14 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  settings = {
+    counter: false,
+    plugins: [lgZoom],
+  };
+
   showOffers = environment.showOffers;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public dialog: MatDialog) {}
 
   ngOnInit(): void {}
 
@@ -22,4 +30,13 @@ export class LandingPageComponent implements OnInit {
   route(page: string): void {
     this.router.navigate([page]);
   }
+
+  openHomepage() {
+    this.router.navigate(['']);
+  }
+
+  openPolicy() {
+    this.dialog.open(PolicyComponent);
+  }
+
 }
