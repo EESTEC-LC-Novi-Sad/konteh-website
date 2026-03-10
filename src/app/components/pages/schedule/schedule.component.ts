f9deed5ec329cb01ccaddb862c0ee5ddc59de163
Stefan Milojevic <stefanmiloje87@gmail.com>
2026-03-10 16:03:48 +0100

    Added scroll position persistence

--- src/app/components/pages/schedule/schedule.component.ts
+++ src/app/components/pages/schedule/schedule.component.ts
@@ -1,10 +1,17 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ScheduleService } from 'src/app/services/schedule.service';
 import { OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Schedule } from 'src/app/model/schedule';
 
+@Injectable({
+  providedIn: 'root',
+})
+export class ScheduleStateService {
+  selectedDate: Date | null = null;
+  scrollPosition: number = 0;
+}
 
 @Component({
   selector: 'schedule',
@@ -47,14 +54,13 @@ export class ScheduleComponent implements OnInit, AfterViewInit {
 
   scheduleLoading: boolean = true;
 
-  selectedDate: Date | null = null;
-
   day1: Date = new Date(2026, 2, 18);
   day2: Date = new Date(2026, 2, 19);
 
   constructor(
     private router: Router,
-    private scheduleService: ScheduleService
+    private scheduleService: ScheduleService,
+    public scheduleStateService: ScheduleStateService
   ) { }
 
   ngOnInit(): void {
@@ -64,12 +70,20 @@ export class ScheduleComponent implements OnInit, AfterViewInit {
       this.filteredSchedules = this.schedules;
       this.scheduleLoading = false;
 
-      this.filterSchedule(this.day1)
+      if (this.scheduleStateService.selectedDate) {
+        this.filterSchedule(this.scheduleStateService.selectedDate);
+      } else {
+        this.filterSchedule(this.day1);
+      }
+
+      setTimeout(() => {
+        window.scrollTo(0, this.scheduleStateService.scrollPosition);
+      }, 0);
     });
   }
 
   filterSchedule(date: Date) {
-    this.selectedDate = date;
+    this.scheduleStateService.selectedDate = date;
     this.filteredSchedules = this.schedules.filter((schedule) => new Date(schedule.startTime).getDate() === date.getDate());
   }
 
